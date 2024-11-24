991f941f84b73dad878dae9c49fcdf69a4d73214
giddy11 <edoghotugiddy@gmail.com>
2024-11-24 16:41:52 +0100

    up

--- backend/models/routerPermission.js
+++ backend/models/routerPermission.js
@@ -8,7 +8,7 @@ const routerPermissionSchema = new Schema(
     permission_id: {type: mongoose.Schema.Types.ObjectId, required: true, ref:'Permission'},
     permission: {type: Array, required: true } // 0,1,2,3
   },
-  { timestamps: true }
+  { timestamps: true } 
 );
 
 module.exports = mongoose.model("RouterPermission", routerPermissionSchema);
