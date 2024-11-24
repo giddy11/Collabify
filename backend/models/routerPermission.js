f96407a8e64eea9a50f14e7c0e1677f9daf52287
giddy11 <edoghotugiddy@gmail.com>
2024-11-24 16:41:01 +0100

    updared

--- backend/models/routerPermission.js
+++ backend/models/routerPermission.js
@@ -5,6 +5,7 @@ const routerPermissionSchema = new Schema(
   {
     router_endpoint: { type: String, required: true },
     role: {type: Number, required: true }, // 0, 1
+    permission_id: {type: mongoose.Schema.Types.ObjectId, required: true, ref:'Permission'},
     permission: {type: Array, required: true } // 0,1,2,3
   },
   { timestamps: true }
