f198f387103444762b1ab441eb3bb9cb7de87bf7
Edoghotu Gideon Azibaobuom <gideon.edoghotu@cyphercrescent.com>
2024-11-25 11:29:35 +0100

    updated

--- backend/routes/authRoutes.js
+++ backend/routes/authRoutes.js
@@ -1,11 +1,20 @@
 const express = require('express');
+const rateLimit = require('express-rate-limit');
+
 const { signup, login, forgotPassword, changePassword, logout, googleLogin, refreshAccessToken } = require('../controllers/authController');
 const authMiddleware = require('../middlewares/authMiddleware');
 const { registerValidator, loginValidator } = require('../validators/authValidator');
 const router = express.Router();
 
-router.post('/signup', registerValidator, signup);
-router.post('/login', loginValidator, login);
+// Set up rate limiter
+const loginLimiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 minutes
+    max: 5, // Limit each IP to 5 requests per windowMs
+    message: 'Too many login attempts, please try again later.',
+  });
+
+router.post('/signup', registerValidator, loginLimiter, signup);
+router.post('/login', loginValidator, loginLimiter, login);
 router.post('/forgot-password', forgotPassword);
 router.post('/change-password', authMiddleware, changePassword);
 router.post('/logout', logout);
